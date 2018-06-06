5ade611dec733c50d1604118ea32033c6ffb4915
landrygears3 <landry_1995@live.com.mx>
2018-06-06 13:33:08 -0500

    Notas
    Agregado de notas a cada paquete.

--- PVenta/src/Vista/Mods/ModsB.java
+++ PVenta/src/Vista/Mods/ModsB.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Vista;
+package Vista.Mods;
 
 /**
  *
  * @author landr
  */
-public class Mods {
+public class ModsB {
     
 }
