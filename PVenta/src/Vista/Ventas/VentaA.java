ad2c3086329b8670c2ee48ed9aa5c13d7c3117d0
RLandryD <39928833+RLandryD@users.noreply.github.com>
2018-06-15 21:02:56 -0500

    Ventasv2
    Nuevo de ventas

--- PVenta/src/Vista/Ventas/VentaA.java
+++ PVenta/src/Vista/Ventas/VentaA.java
@@ -22,9 +22,9 @@ public class VentaA extends VistasGenerales.Panel {
     public JLabel id, t, tp, mo, col, des, pv, can,
             im, ma, su, pi, PRO, IM, nc, cc;
     
-    private JLabel v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12,
-            v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23;
-    public JTextField ID, CAN, PV, NC, CC;
+    private JLabel v2, v3, v4, v5, v6, v10, v11, v13,
+            v14, v15, v16, v17, v18, v19, v20, v21, v22;
+    public JTextField CAN, PV, NC, CC;
     public String Prod[] = {"Tipo de producto"};
     public String Pago[] = {"Tipo de pago"};
     public String Mod[] = {"Modelo"};
@@ -32,7 +32,7 @@ public class VentaA extends VistasGenerales.Panel {
     public String Des[] = {"Descripción"};
     public String Suc[] = {"Sucursal"};
     public String piel[] = {"Tipo de piel"};
-    public JComboBox T, TP, MO, COL, DES, SU, PI;
+    public JComboBox T, TP, MO, COL, DES, SU, PI, ID;
     JComboBox idproducto;
     public Panel P = new Panel();
     public Panel Pr = new Panel();
@@ -53,22 +53,19 @@ public class VentaA extends VistasGenerales.Panel {
     }
 
     private void crea() {
-        P.setLayout(new java.awt.GridLayout(6, 2));
-        Pb.setLayout(new java.awt.GridLayout(7, 3));
+        P.setLayout(new java.awt.GridBagLayout());
+        Pb.setLayout(new java.awt.GridBagLayout());
         Pb.setAlignmentX(CENTER_ALIGNMENT);
         Pr.setLayout(new GridBagLayout());
-        v1 = new JLabel();      v2 = new JLabel();
-        v3 = new JLabel();      v4 = new JLabel();
-        v5 = new JLabel();      v6 = new JLabel();
-        v7 = new JLabel();      v8 = new JLabel();
-        v9 = new JLabel();      v10 = new JLabel();
-        v11 = new JLabel();      v12 = new JLabel();
+        v2 = new JLabel();      v3 = new JLabel();      
+        v4 = new JLabel();      v5 = new JLabel();
+        v6 = new JLabel();      v10 = new JLabel();
+        v11 = new JLabel();      
         v13 = new JLabel();      v14 = new JLabel();
         v15 = new JLabel();      v16 = new JLabel();
         v17 = new JLabel();      v18 = new JLabel();
         v19 = new JLabel();      v20 = new JLabel();
         v21 = new JLabel();      v22 = new JLabel();
-        v23 = new JLabel();
         MA = new JCheckBox();
         id = new JLabel("ID");                  //id venta
         pv = new JLabel("Precio de venta");
@@ -78,7 +75,7 @@ public class VentaA extends VistasGenerales.Panel {
         im = new JLabel("(Insertar imagen)");
         im.setHorizontalAlignment(JLabel.CENTER);
         ma = new JLabel("Mayoreo");             //CheckBox
-        ID = new JTextField(10);
+        ID = new JComboBox();
         CAN = new JTextField(10);
         PV = new JTextField(10);
         NC = new JTextField(10);
@@ -115,47 +112,38 @@ public class VentaA extends VistasGenerales.Panel {
     private void agrega() {
 
         
-        P.add(id);
-        P.add(ID);
-        P.add(pv);
-        P.add(PV);
-        P.add(can);
-        P.add(CAN);
-        P.add(nc);
-        P.add(NC);
-        P.add(cc);
-        P.add(CC);
-        P.add(ma);
-        P.add(MA);
-        
-        Pb.add(v15);
+        P.add(id, estilo(0,0,1,1));
+        P.add(ID, estilo(1,0,1,1));
+        P.add(pv, estilo(2,0,1,1));
+        P.add(PV, estilo(3,0,1,1));
+        P.add(v3, estilo(0,1,4,1));
+        P.add(can, estilo(0,2,1,1));
+        P.add(CAN, estilo(1,2,1,1));
+        P.add(nc, estilo(2,2,1,1));
+        P.add(NC, estilo(3,2,1,1));
+        P.add(v4, estilo(0,3,4,1));
+        P.add(cc, estilo(0,4,1,1));
+        P.add(CC, estilo(1,4,1,1));
+        P.add(ma, estilo(2,4,1,1));
+        P.add(MA, estilo(3,4,1,1));
+        P.add(v11, estilo (0,5,4,1));
         Pb.add(T);
-        Pb.add(v16);
-        Pb.add(v19);
         Pb.add(TP);
-        Pb.add(v17);
-        Pb.add(v18);
         Pb.add(MO);
-        Pb.add(v20);
-        Pb.add(v2);
-        Pb.add(COL);
-        Pb.add(v4);
-        Pb.add(v21);
-        Pb.add(DES);
-        Pb.add(v14);
-        Pb.add(v10);
-        Pb.add(SU);
-        Pb.add(v11);
         Pb.add(v13);
+        Pb.add(COL);
+        Pb.add(DES);
+        Pb.add(SU);
+        Pb.add(v14);
         Pb.add(PI);
-        Pb.add(v22);
         
         
-        Pr.add(v6, estilo(0, 0, 1, 1));
-        Pr.add(P, estilo (1, 0, 1, 1));
+        
+        //Pr.add(v6, estilo(0, 0, 1, 1));
+        Pr.add(P, estilo (0, 0, 1, 1));
         Pr.add(im, estilo(2, 0, 1, 1));
-        Pr.add(v3, estilo(0, 1, 1, 1));
-        Pr.add(Pb, estilo(1, 1, 1, 1));
+        //Pr.add(v3, estilo(0, 1, 1, 1));
+        Pr.add(Pb, estilo(0, 1, 1, 1));
         Pr.add(v5, estilo(2, 1, 1, 1));
         Pr.add(tab, estilo(0, 2, 3, 1));
         validate();
