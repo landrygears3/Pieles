5ad1b864ac6408588fe77ded3620848e7da7d54c
RLandryD <39928833+RLandryD@users.noreply.github.com>
2018-07-01 23:14:04 -0500

    EmModifica
    Agregando las contraseñas y horas.

--- PVenta/src/Vista/Empleado/EmpleadoM.java
+++ PVenta/src/Vista/Empleado/EmpleadoM.java
@@ -5,12 +5,14 @@
  */
 package Vista.Empleado;
 
+import VistasGenerales.Contrasena;
 import VistasGenerales.Panel;
 import com.toedter.calendar.JDateChooser;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -26,15 +28,19 @@ public class EmpleadoM extends VistasGenerales.Panel{
     final String ns[] = {"Nombre"};
     VistasGenerales.Tabla tab;
     JButton c, ca;
+    public Contrasena C, CC;
     JLabel u, no, v1, v2, p, v3, v4, v5, v6, va, vb, vc, vd, ve, vf,
-            el, vz, vy, vx, vw, vv, vu;
+            el, vz, vy, vx, vw, vv, vu, v7, v8,cn,cc;
     JTextField U, NO, P;
     JComboBox n;
+    Panel hora;
+    Panel contrasenas;
     public Panel Pr = new Panel();
     public Panel Pb = new Panel();
     Container co2 = new Container();
     Container co = new Container();
     Container ti = new Container();
+    Container cf = new Container();
     VistasGenerales.Number CAN;
     VistasGenerales.TimeChoser horae, horas;
     JDateChooser fecha;
@@ -56,6 +62,10 @@ public class EmpleadoM extends VistasGenerales.Panel{
         c = new JButton ("Modificar usuario");
         el = new JLabel (" ");
         ca = new JButton("Cancelar");
+        cn = new JLabel("Nueva");
+        cc = new JLabel("Confirmar");
+        C = new Contrasena();
+        CC = new Contrasena();
         
         U = new JTextField(10);
         CAN = new VistasGenerales.Number();
@@ -71,12 +81,23 @@ public class EmpleadoM extends VistasGenerales.Panel{
         v1 = new JLabel(" ");       v2 = new JLabel(" ");
         v3 = new JLabel(" ");       v4 = new JLabel(" ");
         v5 = new JLabel(" ");       v6 = new JLabel(" ");
+        v7 = new JLabel(" ");       v8 = new JLabel(" ");
         va = new JLabel(" ");       vb = new JLabel(" ");
         vc = new JLabel(" ");       vd = new JLabel(" ");
         ve = new JLabel(" ");       vf = new JLabel(" ");
         vz = new JLabel(" ");       vy = new JLabel(" ");
         vu = new JLabel(" ");       vv = new JLabel(" ");
         vw = new JLabel(" ");       vx = new JLabel(" ");
+        
+        horae = new VistasGenerales.TimeChoser();
+        horas = new VistasGenerales.TimeChoser();
+        fecha = new JDateChooser();
+        hora = new Panel();
+        hora.setBorder(BorderFactory.createTitledBorder("Horario"));
+        hora.setLayout(new GridBagLayout());
+        contrasenas = new Panel();
+        contrasenas.setBorder(BorderFactory.createTitledBorder("Contraseña"));
+        contrasenas.setLayout(new GridBagLayout());
         
     tab = new VistasGenerales.Tabla();
         tab.setColum(cols);
@@ -85,6 +106,7 @@ public class EmpleadoM extends VistasGenerales.Panel{
         Pb.setLayout(new GridLayout(1,2));
         co2.setLayout(new GridLayout(7,1));
         co.setLayout(new GridLayout(8,1));
+        cf.setLayout(new GridLayout(1,3));
         ti.setLayout(new GridBagLayout());
     }
     
@@ -101,19 +123,34 @@ public class EmpleadoM extends VistasGenerales.Panel{
 
         return gbc;
     }
+    
+    
+    private GridBagConstraints est2(int pox, int poy, int tax, int tay, int fill, int anchor) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = pox;
+        gbc.gridy = poy;
+        gbc.gridwidth = tax;
+        gbc.gridheight = tay;
+        gbc.fill = fill;
+        gbc.anchor = anchor;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+
+        return gbc;
+    }
 
     
     private void carga(){
         
+        
         ti.add(vx, estilo(0,0,3,1));
-        ti.add(vz, estilo(0,1,1,1));
+        ti.add(vy, estilo(0,1,1,1));
         ti.add(fecha, estilo(1,1,1,1));
-        ti.add(vw, estilo(0,2,3,1));
+        ti.add(vz, estilo(0,2,3,1));
         ti.add(horae, estilo(1,3,1,1));
-        ti.add(vy, estilo(2,3,1,1));
-        ti.add(vv, estilo(0,4,3,1));
+        ti.add(vu, estilo(0,4,3,1));
         ti.add(horas, estilo(1,5,1,1));
-        ti.add(vu, estilo(0,6,3,1));
+        ti.add(vv, estilo(0,6,3,1));
         
         co.add(n);
         co.add(u);
@@ -122,23 +159,40 @@ public class EmpleadoM extends VistasGenerales.Panel{
         co.add(P);
         co.add(no);
         co.add(CAN.tf);
-        co.add(v2);
+        co.add(v1);
         
-        co2.add(v3);
+        co2.add(v2);
         co2.add(el);
-        co2.add(v4);
+        co2.add(v3);
         co2.add(c);
-        co2.add(v5);
+        co2.add(v4);
         co2.add(ca);
-        co2.add(v6);
+        co2.add(v5);
         
-        Pb.add(co);
-        Pb.add(co2);
+        v1 = new JLabel(" ");       v2 = new JLabel(" ");
+        v3 = new JLabel(" ");       v4 = new JLabel(" ");
+        v5 = new JLabel(" ");
         
-        Pr.add(va, estilo(0,0,3,1));
-        Pr.add(vd, estilo(0,1,1,1));
+        contrasenas.add(v1, est2(0, 0, 3, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER));
+        contrasenas.add(cn, est2(0, 1, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER));
+        contrasenas.add(v2, est2(0, 2, 3, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER));
+        contrasenas.add(C, est2(0, 3, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER));
+        contrasenas.add(v3, est2(0, 4, 3, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER));
+        contrasenas.add(cc, est2(0, 5, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER));
+        contrasenas.add(v4, est2(0, 6, 3, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER));
+        contrasenas.add(CC, est2(0, 7, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER));
+        contrasenas.add(v5, est2(0, 8, 3, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER));
+        
+        cf.add(v6);
+        cf.add(co2);
+        cf.add(v7);
+        
+        Pb.add(contrasenas);
+        Pb.add(ti);
+        
+        Pr.add(co, estilo(0,1,1,1));
         Pr.add(Pb, estilo(1,1,1,1));
-        Pr.add(ti, estilo(2,1,1,1));
+        Pr.add(cf, estilo(2,1,1,1));
         Pr.add(tab, estilo(0,2,3,1));
         
     }
