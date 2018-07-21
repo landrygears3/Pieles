cbf80ab58e531d379e0e043d939d9c95b6ae5aa0
landrygears3 <landry_1995@live.com.mx>
2018-07-21 18:59:08 -0500

    Notas

--- PVenta/src/Modelo/Conexion.java
+++ PVenta/src/Modelo/Conexion.java
@@ -35,6 +35,32 @@ public class Conexion {
         }
     }
 
+    /*
+    getConsultas regresa una variable tipo resulset como parametrso
+    de entrada nececita la tabla a la que será consultado, los campos que se
+    deben consultar (en caso de ser todos el parametro de campos es "*"
+    la condición s emanda "" vacia si no hay. Si hay se pone el where o la condición ocupada
+    
+    para manejar el metodo se manda a llmanar de la siguente manera
+    
+    Lista de listas
+    ArrayList<ArrayList> array=new ArrayList<ArrayList>();
+    
+    try{
+    ResultSet variable= Conexion.getConsulta("tabla","*",""); 
+    while(variable.next()){
+    array.add(new ArrayList());
+    
+    los numeros que van en el add depende de la cantidad de columnas que se pidieron
+    array.get(array.size()-1).add(variable.getString(1));
+    array.get(array.size()-1).add(variable.getString(2));
+    
+    }
+    }catch(Exception e){
+    
+    }
+    
+     */
     public ResultSet getConsulta(String tabla, String campos, String condicion) {
         Statement s;
         ResultSet r = null;
@@ -43,19 +69,24 @@ public class Conexion {
             s = connection.createStatement();
             String query2 = "SELECT " + campos + " FROM `" + tabla + "`" + condicion;
             r = s.executeQuery(query2);
-           
+
             return r;
 
         } catch (SQLException ex) {
             System.out.println(ex);
             return null;
         }
-        
-        
-    }
-    
-    
 
+    }
+
+    /*
+    Alta se manda a llamar con el mismo objeto de conexion recibe de parametros la tabla 
+    igual que getConsulta. los campos se mandan separados por comas "campo1, campo2, campo3"
+    los valores igual "valor1, valor2, valor3"
+    
+    Conexion.Alta("tabla","campo1, campo2, campo3","valor1, valor2, valor3"); 
+    
+     */
     public void Alta(String tabla, String campos, String valores) {
 
         try {
@@ -66,7 +97,7 @@ public class Conexion {
             this.closed();
 
         } catch (SQLException ex) {
-            System.err.println("Error"+ex);
+            System.err.println("Error" + ex);
         }
     }
 }
